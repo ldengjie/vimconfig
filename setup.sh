8cfbfd40cb4b8f3b8114cd0f7c7fd0fbe8d8d9f1
ldengjie <ldengjie@163.com>
2018-07-01 03:02:46 +0800

    s

--- setup.sh
+++ setup.sh
@@ -10,24 +10,24 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 ln -s ~/.vim/zshrc ~/.zshrc
 
 # tmux:
-sudo apt-get install tmux
+sudo apt-get -y install tmux
 ln -s ~/.vim/tmux.conf ~/.tmux.conf
 
 # eclim:
 #自带 c/c++/python/java
 
 # Ag:
-sudo apt-get install silversearcher-ag
+sudo apt-get -y install silversearcher-ag
 
 # ctags:
-sudo apt-get install ctags
+sudo apt-get -y install ctags
 
 # vim:
 #<https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source>
 
 sudo apt remove vim vim-runtime gvim
 
-sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+sudo apt -y install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
 
 git clone https://github.com/vim/vim --depth 1
 
@@ -35,14 +35,14 @@ cd ~/.vim/vim
 
 # 修改 src/screen.c, 替换 ~ ^ 为空格
 
-sudo apt-get install libncurses5-dev
+sudo apt-get -y install libncurses5-dev
 
-# 需要配置python3路径
-# [ubuntu16.04]
+# 需要保证编译vim看到的或指定的python Library和编译ycm时看到的python/python-config是同一套，且python是一个可执行文件.详见 http://bc28335b.wiz03.com/share/s/2Ya3dr0qkABL2n0eR71ISkyG0fl6W-389A5l2n46HL3jUW39
+
+# [ubuntu16.04] 需要配置python2/3路径
 ./configure --with-features=huge --enable-python3interp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ --enable-rubyinterp --enable-luainterp --enable-perlinterp --with-python3-config-dir=/home/sugon/anaconda3/lib/python3.6/config-3.6m-x86_64-linux-gnu --enable-multibyte --enable-cscope
-
-# [mac] 需要保证编译vim看到的或指定的python Library和编译ycm时看到的python/python-config是同一套，且python是一个可执行文件.详见 http://bc28335b.wiz03.com/share/s/2Ya3dr0qkABL2n0eR71ISkyG0fl6W-389A5l2n46HL3jUW39
-./configure --with-features=huge --enable-python3interp --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp --enable-multibyte --enable-cscope
+# [mac] 
+#./configure --with-features=huge --enable-python3interp --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp --enable-multibyte --enable-cscope
 
 make && sudo make install
 
@@ -50,11 +50,12 @@ ln -s ~/.vim/vimrc ~/.vimrc
 
 cd ~/.vim/plugged/
 
-git config --global http.postBuffer 1024288000
-git clone --depth 1 --recursive https://github.com/Valloric/YouCompleteMe.git
-cd YouCompleteMe
-sudo apt-get install build-essential cmake
-./install.py --all
+# ycm
+#git config --global http.postBuffer 1024288000
+#git clone --depth 1 --recursive https://github.com/Valloric/YouCompleteMe.git
+#cd YouCompleteMe
+#sudo apt-get -y install build-essential cmake
+#./install.py --all
 
 vim -c 'PlugInstall' -c 'qall'
 
